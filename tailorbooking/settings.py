0025a694f79a30a9f4623880167a62a6aa60fa35
Siddhant Patil <shimpli.patil2804@gmail.com>
2022-12-06 14:32:04 +0530

    Update settings.py

--- tailorbooking/settings.py
+++ tailorbooking/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = "tailorbooking.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "BASE_DIR / 'db.sqlite3'",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "BASE_DIR / 'db.sqlite3'",
+#     }
+# }
 
 
 DATABASES['default'] = dj_database_url.config()
